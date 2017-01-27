eb87758d2bddb1270d99ee030b1f9afaf16ed61a
Eliot Hill <eliot.hill@digital.homeoffice.gov.uk>
2017-01-27 15:47:37 +0000

    add: all the things

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,12 @@ var gulp = require('gulp');
 
 gulp.task('hello', function() {
   console.log('Hello Zell');
-});
+});	
+
+const zip = require('gulp-zip');
+
+gulp.task('zzz', () =>
+    gulp.src('app/assets/*')
+        .pipe(zip('archive.zip'))
+        .pipe(gulp.dest('dist'))
+);
